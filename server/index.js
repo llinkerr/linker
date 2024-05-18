5ad36395e2e2b7627e19f0cb9c3fa73b25a86201
vishal <vishalkesharwani1406@gmail.com>
2024-05-18 15:27:34 +0530

    database added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+require('dotenv').config();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Connect to MongoDB
+mongoose.connect(process.env.URI,)
+  .then(() => {
+    console.log('Database connected');
+    // Start the Express server after successful database connection
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Database connection error:', error);
+  });
